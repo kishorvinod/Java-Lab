ed9d8cf2fbc15c2005bcf7289936360988ece4cc
kishorvinod <117641547+kishorvinod@users.noreply.github.com>
2023-06-07 15:45:11 +0530

    Add files via upload

---
+++ cycle 2/SearchElement.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class SearchElement {
+         public static void main(String[] args){
+         
+         System.out.println("Kishor Vinod");
+         System.out.println("SJC22MCA-2035");
+         System.out.println("04-04-2023");
+         System.out.println("20MCA132 , Object Oriented Programming Lab \n\n");
+
+         int n, x, flag = 0, i= 0;
+         Scanner s = new Scanner(System.in);
+         System.out.print("Enter no. of elements you want in array:");
+         n = s.nextInt();
+         int a[] = new int[n];
+         System.out.println("Enter all the elements:");
+         for(i = 0; i < n; i++){
+                a[i] = s.nextInt();
+         }
+         System.out.print("Enter the element you want to find:");
+         x = s.nextInt();
+         for(i = 0; i < n; i++){
+              if(a[i] == x){
+                  flag = 1;
+                    break;
+              }
+              else{
+                flag = 0;
+              }
+        }
+        if(flag == 1){
+           System.out.println("Element found at position:"+(i + 1));
+        }
+        else{
+          System.out.println("Element not found");
+      }
+   }
+}
